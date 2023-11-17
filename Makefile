e4cbf26ce94ce67d07634c3ad42a2240178fd0d5
Gaurav Rathi <51204874+gaurav0720@users.noreply.github.com>
2023-11-17 01:32:14 +0000

    after running project

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 CC = g++
 CXXFLAGS = -Wall -g
 BINARY = atm2000
-OBJECTS = atm2000.o BankAccount.o CheckingAccount.o SavingAccount.o
+SOURCES = atm2000.cpp BankAccount.cpp CheckingAccount.cpp SavingAccount.cpp
+OBJECTS = $(SOURCES:.cpp=.o)
+
+all: $(BINARY)
 
 $(BINARY): $(OBJECTS)
-    $(CC) $(OBJECTS) -o $(BINARY)
+	$(CC) $(OBJECTS) -o $(BINARY)
 
 %.o: %.cpp
-    $(CC) $(CXXFLAGS) -c $<
+	$(CC) $(CXXFLAGS) -c $<
 
 clean:
-    rm -f *.o $(BINARY)
+	rm -f $(OBJECTS) $(BINARY)
--- atm2000.cpp
+++ atm2000.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 // Assuming getTransaction is a function that gets the transaction type, amount, and account type
-void getTransaction(char &transact, double &amount, char &fromAcct);
+// void getTransaction(char &transact, double &amount, char &fromAcct);
 
 int main() {
     SavingAccount savings;
@@ -30,31 +30,49 @@ int main() {
     savings.update();
 
     do {
+        cout << "\n*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-*+-\n";
         cout << "What would you like to do today?\n";
         cout << "B)alance, D)eposit, W)ithdraw, C)heck, T)ransfer, Q)uit?\n";
-        getTransaction(transact, amount, fromAcct);
+        // getTransaction(transact, amount, fromAcct);
+        cin >> transact;
 
         switch (transact) {
             case 'B': // Balance
+                cout << "From which account? (C)hecking (S)aving: "<<endl;
+                cin >> fromAcct;
                 if (fromAcct == 'C') checking.output(cout);
                 else if (fromAcct == 'S') savings.output(cout);
                 break;
 
             case 'D': // Deposit
+                cout << "What amount would you like to Deposit today? "<<endl;
+                cin >> amount;
+                cout << "From which account? (C)hecking (S)aving: "<<endl;
+                cin >> fromAcct;
                 if (fromAcct == 'C') checking.deposit(amount);
                 else if (fromAcct == 'S') savings.deposit(amount);
                 break;
 
             case 'W': // Withdraw
+                cout << "What amount would you like to Withdraw today? "<<endl;
+                cin >> amount;
+                cout << "From which account? (C)hecking (S)aving: "<<endl;
+                cin >> fromAcct;
                 if (fromAcct == 'C') checking.withdraw(amount);
                 else if (fromAcct == 'S') savings.withdraw(amount);
                 break;
 
             case 'C': // Check
+                cout << "What amount would you like to Check today? "<<endl;
+                cin >> amount;
                 if (fromAcct == 'C') checking.check(amount);
                 break;
 
             case 'T': // Transfer
+                cout << "What amount would you like to Transfer today? "<<endl;
+                cin >> amount;
+                cout << "From which account? (C)hecking (S)aving: "<<endl;
+                cin >> fromAcct;
                 if (fromAcct == 'C') {
                     checking.withdraw(amount);
                     savings.deposit(amount);
